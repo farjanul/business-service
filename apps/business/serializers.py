35cc74d28ece98b03a6210024a8ececd393b4395
farjanul <farjanul@synesisit.info>
2023-07-18 18:42:20 +0600

    serializer change for uuid

--- apps/business/serializers.py
+++ apps/business/serializers.py
@@ -5,4 +5,4 @@ from apps.business.models import Business
 class BusinessSerializer(serializers.ModelSerializer):
     class Meta:
         model = Business
-        fields = ['id', 'name', 'latitude', 'longitude']
+        fields = ['uuid', 'name', 'latitude', 'longitude']
